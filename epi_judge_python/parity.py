758fb4465a7842d2a687c9eebd8999c27df80515
Misael Valentin <misael.vf2@gmail.com>
2024-02-20 22:40:27 -0500

    Optimize parity.py (second pass)

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,18 +1,24 @@
 from test_framework import generic_test
 
 
+CHUNK_SIZE = 16
+
+
 def parity(x: int) -> int:
-    i = 0
-    answer = 0
-    while i < 64:
-        answer ^= PARITY_TABLE[(x >> i) & 0xFFFF]
-        i += 16
-    return answer
+    result = 0
+    # This neat trick gives me a bit mask consisting of CHUNK_SIZE 1 bits
+    bit_mask = (1 << CHUNK_SIZE) - 1
+    while x:
+        # This uses masking to isolate the last CHUNK_SIZE bits
+        result ^= PARITY_TABLE[x & bit_mask]
+        # This shifts the next CHUNK_SIZE bits to the end of the word
+        x >>= CHUNK_SIZE
+    return result
 
 
 def build_parity_table(size: int) -> dict[int, int]:
     table = {}
-    for i in range(2**size):
+    for i in range(1 << size + 1):
         table[i] = count_set_bits(i) % 2
     return table
 
@@ -20,12 +26,14 @@ def build_parity_table(size: int) -> dict[int, int]:
 def count_set_bits(x: int) -> int:
     count = 0
     while x:
-        count += x & 1
-        x >>= 1
+        count += 1
+        # This bit manipulation trick clears the lowest set bit,
+        # so this loop runs in O(k), where k is the number of set bits
+        x &= x - 1
     return count
 
 
-PARITY_TABLE = build_parity_table(16)
+PARITY_TABLE = build_parity_table(CHUNK_SIZE)
 
 
 if __name__ == "__main__":
