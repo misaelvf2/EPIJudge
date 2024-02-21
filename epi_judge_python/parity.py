d94e61330d448d8438bfc812ab5773be12b89e10
Misael Valentin <misael.vf2@gmail.com>
2024-02-20 23:20:20 -0500

    Optimize parity.py (final optimization)
    Add and describe all solution variations.

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -5,14 +5,65 @@ CHUNK_SIZE = 16
 
 
 def parity(x: int) -> int:
+    # This approach combines the word-level XOR instructions and caching
+    x ^= x >> 32
+    return PARITY_TABLE[(x >> 16) & 0xFFFF] ^ PARITY_TABLE[x & 0xFFFF]
+
+
+def parity_word_level(x: int) -> int:
+    # Time: O(log n)
+    # Space: O(1)
+    # This exploits the fact that XOR is associative and commutative
+    # to use CPU word-level XOR instructions
+    # The running XOR is stored in the latter half of the word,
+    # and we continue until it reaches a single bit,
+    # at which point, we extract it from the word
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 1
+
+
+def parity_cache(x: int) -> int:
+    # Time: O(n/L), where L is the CHUNK_SIZE
+    # Space: O(1), ignoring space used for table
+    # This approach is based on the observation that XOR is associative,
+    # so we can split the input into n/L CHUNK_SIZE sub-words
+    # If we pre-compute a table for every CHUNK_SIZE sub-word,
+    # then this algorithm reduces to keying into the table
+    # and combining the sub-results
+    # This is workable for 16-bit subwords, since the table
+    # only takes up 64KiB
     result = 0
-    # This neat trick gives me a bit mask consisting of CHUNK_SIZE 1 bits
     bit_mask = (1 << CHUNK_SIZE) - 1
     while x:
-        # This uses masking to isolate the last CHUNK_SIZE bits
         result ^= PARITY_TABLE[x & bit_mask]
-        # This shifts the next CHUNK_SIZE bits to the end of the word
         x >>= CHUNK_SIZE
+    return result
+
+
+def parity_unoptimized(x: int) -> int:
+    # Time: O(n)
+    # Space: O(1)
+    # Unoptimized algorithm. Look at each bit, counting the 1s as you go
+    result = 0
+    while x:
+        result ^= x & 1
+        x >>= 1
+    return result
+
+
+def parity_clear_lowest(x: int) -> int:
+    # Time: O(k)
+    # Space: O(1)
+    # Use bit-flidding trick to only look at the set bits
+    result = 0
+    while x:
+        result ^= 1
+        x &= x - 1
     return result
 
 
@@ -27,7 +78,7 @@ def count_set_bits(x: int) -> int:
     count = 0
     while x:
         count += 1
-        # This bit manipulation trick clears the lowest set bit,
+        # This bit-fiddling trick clears the lowest set bit,
         # so this loop runs in O(k), where k is the number of set bits
         x &= x - 1
     return count
