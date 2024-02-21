8f433b820cf73df1dc95f41cc85a4186482c3200
Misael Valentin <misael.vf2@gmail.com>
2024-02-20 22:03:48 -0500

    Optimize parity.py (first pass)

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,12 +2,31 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
+    i = 0
+    answer = 0
+    while i < 64:
+        answer ^= PARITY_TABLE[(x >> i) & 0xFFFF]
+        i += 16
+    return answer
+
+
+def build_parity_table(size: int) -> dict[int, int]:
+    table = {}
+    for i in range(2**size):
+        table[i] = count_set_bits(i) % 2
+    return table
+
+
+def count_set_bits(x: int) -> int:
     count = 0
     while x:
-        count ^= x & 1
-        x = x >> 1
+        count += x & 1
+        x >>= 1
     return count
 
 
-if __name__ == '__main__':
-    exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
+PARITY_TABLE = build_parity_table(16)
+
+
+if __name__ == "__main__":
+    exit(generic_test.generic_test_main("parity.py", "parity.tsv", parity))
