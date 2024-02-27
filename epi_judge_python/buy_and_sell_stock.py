c848b5b1d2451de34257a745dfa7515268eb1daf
Misael Valentin <misael.vf2@gmail.com>
2024-02-26 23:04:11 -0500

    Solve buy_and_sell_stock_once.py (complete search)

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,7 +4,18 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
+    # Brute-force / Complete search approach.
+    # Check potential profit for all (buy, sell) price pairs,
+    # where the sell price must after the buy price.
+    # Time: O(n^2)
+    # Space: O(1)
     result = 0.0
+
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            result = max(result, prices[j] - prices[i])
+
+    return result
 
 
 if __name__ == "__main__":
